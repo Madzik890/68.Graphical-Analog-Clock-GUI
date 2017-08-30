3c84fad87da6d1840179533cfec0d513c0215fc8
Marcin Sucharski <Madzik890@hotmail.com>
2017-08-30 13:55:45 +0200

    Created the whole mechanism of clock(clock hands and face).

--- Analog Clock/Clock.cs
+++ Analog Clock/Clock.cs
@@ -13,50 +13,87 @@ namespace Analog
     {
         public Clock()
         {
-            leads.Add(new Lead(new vector2(100,200),new vector2(100,100),Color.Chartreuse,3));
-          /*  leads.Add(new Lead());
-            leads.Add(new Lead());*/
         }
 
-        public List<Lead> leads = new List<Lead>();
+        public List<Hand> hands = new List<Hand>();
+        public Face face = new Face(new vector2(165,150),5,100,Color.Chocolate);
 
-        public virtual void Draw(object sender, PaintEventArgs e)
+        public virtual void Draw(PaintEventArgs e)
         {
-            foreach (Lead value in leads)
+            DrawBackground(e);
+            DrawLeads(e);
+        }
+        public virtual void RecentryOnWindow(Size size)//function which center the clock in window
+        {
+            foreach(Hand value in hands)
+            {
+                value.Begin = new vector2(size.Width/2, size.Height/2);
+            }
+        }   
+       
+        public class Hand//hands of the clock
+        {
+            public Hand() { }//default constructor
+            public Hand(vector2 begin, Color color, float thickness,int radius)
+            {
+                this.begin = begin; this.color = color; this.thickness = thickness; this.radius = radius;
+            }
+
+            public vector2 Begin { set { this.begin = value; } get { return begin; } }
+            public vector2 End { get { return end; } }
+            public Color Color { set { this.color = value; } get { return color; } }
+            public float Radius { set { this.radius = value; } get { return radius; } }
+            public float thickness = 3;
+            public double rotation = 0;//rotation lead
+
+            public virtual void CalculateAngel()//public function which calculate shape of lead for the given angel   
+            {
+                end.x = Convert.ToInt32(Begin.x + radius * Math.Sin(rotation * Math.PI / 180));
+                end.y = Convert.ToInt32(Begin.y - radius * Math.Cos(rotation * Math.PI / 180));
+            }
+
+            private Color color = new Color();//default color
+            private vector2 begin = new vector2(100, 200);//default position
+            private vector2 end = new vector2(0, 0);//second point of line who will be automatical counting 
+            private float radius = 100;//radius for counting end_vector(last point) via angel
+        }
+        public class Face//class who create clock face
+        {
+            public Face() { }
+            public Face(vector2 pos, int thickness, float radius, Color color)
+            {
+                this.pos = pos;this.thickness = thickness; this.radius = radius; this.color = color;
+            }
+
+            public Color color = new Color();
+            public vector2 pos = new vector2(165, 150);
+            public int thickness = 3;
+            public float radius = 100;
+        }
+
+        private vector2 position = new vector2(165, 150);
+
+        private void DrawLeads(PaintEventArgs e)
+        {
+            foreach (Hand value in hands)
             {
                 Pen pen = new Pen(value.Color, value.thickness);
                 // Create points that define line.
+                value.CalculateAngel();
+
                 Point point1 = new Point(value.Begin.x, value.Begin.y);
                 Point point2 = new Point(value.End.x, value.End.y);
 
                 // Draw line to screen.
                 e.Graphics.DrawLine(pen, point1, point2);
             }
+
         }
-        public virtual void Recentry(Size size)//function which center the clock in window
+        private void DrawBackground(PaintEventArgs e)
         {
-            foreach(Lead value in leads)
-            {
-                value.Begin = new vector2(size.Width/2, size.Height/2);
-            }
-        }   
-        public class Lead//hands of the clock
-        {
-            public Lead() { }//default constructor
-            public Lead(vector2 begin, vector2 end, Color color, float thickness)
-            {
-                this.begin = begin; this.end = end; this.color = color; this.thickness = thickness;
-            }
-
-            public vector2 Begin { set { this.begin = value; } get { return begin; } }
-            public vector2 End { set { this.end = value; } get { return end; } }
-            public Color Color { set { this.color = value; } get { return color; } }
-            public float thickness = 3;
-
-
-            private Color color = new Color();
-            private vector2 begin = new vector2(100, 200);
-            private vector2 end = new vector2(100, 100);
+             Pen pen = new Pen(face.color, face.thickness);
+            e.Graphics.DrawEllipse(pen, face.pos.x - face.radius, face.pos.y- face.radius,
+                   face.radius + face.radius, face.radius + face.radius);
         }
     }
 }
--- Analog Clock/Form1.cs
+++ Analog Clock/Form1.cs
@@ -18,11 +18,33 @@ namespace Analog_Clock
         public Form1()
         {
             InitializeComponent();
+            ////////////////creating clock/////////////////////////
+            clock.hands.Add(new Clock.Hand(new Vector.vector2(165, 150), Color.Red, 2, 100));//seconds lead
+            clock.hands.Add(new Clock.Hand(new Vector.vector2(165, 150), Color.Black, 3, 100));//minutes lead
+            clock.hands.Add(new Clock.Hand(new Vector.vector2(165, 150), Color.Black, 3, 70));//hour lead
+            MoveClock();
+            ////////////////creating clock/////////////////////////
+
+            timer1.Start();
         }
 
         private void Display(object sender, PaintEventArgs e)
         {
-            clock.Draw(sender, e);
+            //clock.RecentryOnWindow(this.Size);
+            clock.Draw(e);//drawing clock
+        }
+
+        private void MoveClock()
+        {
+            clock.hands[0].rotation = DateTime.Now.Second * 6;//360 : 60 = 6
+            clock.hands[1].rotation = DateTime.Now.Minute * 6;//6 * x = angel
+            clock.hands[2].rotation = DateTime.Now.Hour * 30;//360 : 12 = 30 30 * x = angel
+            this.Refresh();//redrawing a clock
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MoveClock();
         }
     }
 }
